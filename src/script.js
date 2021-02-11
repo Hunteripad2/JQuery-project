33828f9f7a7b84ddaccd07dd222d8f02d0147873
Juju Man <hunteripad2@gmail.com>
2021-02-11 16:52:03 +0300

    Added bootstrap

--- src/script.js
+++ src/script.js
@@ -1,17 +1,21 @@
-$("form").on("submit", function (event) {
-    console.log("submit");
-    const newTodo = $(".input_text").val();
-    const newTodoItem = `<li><input type="checkbox" onchange='handleChange(this)'><span>${newTodo}</span></li>`;
-    $(".todo_list").append(newTodoItem);
-    $(".input_text").val("");
+$("#newtodo_form").on("submit", function (event) {
+    const newTodo = $("#newtodo_form_text").val();
+    const newTodoItem = `<div class="form-check col-auto todo_item"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div>`;
+
+    if ($(".todo_item").length % 2 === 0) {
+        $("#todo_list").append("<div class='w-100'></div>");
+    }
+
+    $("#todo_list").append(newTodoItem);
+    $("#newtodo_form_text").val("");
 
     event.preventDefault();
 });
 
 function handleChange(checkBox) {
     if (checkBox.checked) {
-        $(checkBox.parentElement).find("span").addClass("checked_item");
+        $(checkBox.parentElement).find("label").addClass("text-decoration-line-through");
     } else {
-        $(checkBox.parentElement).find("span").removeClass("checked_item");
+        $(checkBox.parentElement).find("label").removeClass("text-decoration-line-through");
     }
 }
