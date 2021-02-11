8db99dbb24c0a3f9c49087d13cdd77ed2582b111
Juju Man <hunteripad2@gmail.com>
2021-02-11 17:03:50 +0300

    Some fixes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 $("#newtodo_form").on("submit", function (event) {
     const newTodo = $("#newtodo_form_text").val();
-    const newTodoItem = `<div class="form-check col-auto todo_item"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div>`;
+    const newTodoItem = `<div class="form-check col-auto todo_item" style="margin-top: 2%"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div>`;
 
     if ($(".todo_item").length % 2 === 0) {
         $("#todo_list").append("<div class='w-100'></div>");
