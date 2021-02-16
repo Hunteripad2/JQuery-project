740609e8ba60e2020b2cad6463892f3522e15ff7
Juju Man <hunteripad2@gmail.com>
2021-02-17 02:39:35 +0300

    Added datepicker

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 $("#newtodo_form").on("submit", function (event) {
     const newTodo = $("#newtodo_form_text").val();
-    const newTodoItem = `<div class="form-check col-auto todo_item" style="margin-top: 2%"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div>`;
+    const newTodoItem = `<div class="d-flex flex-row w-auto mb-3"><div class="col-6"><input type="text" class="datepicker form-control-plaintext text-end" placeholder="Введите дату... "></div><div class="form-check col-auto todo_item" style="margin-top: 2%"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div></div>`;
 
     if ($(".todo_item").length % 2 === 0) {
         $("#todo_list").append("<div class='w-100'></div>");
@@ -8,6 +8,7 @@ $("#newtodo_form").on("submit", function (event) {
 
     $("#todo_list").append(newTodoItem);
     $("#newtodo_form_text").val("");
+	$(".datepicker").datepicker();
 
     event.preventDefault();
 });
