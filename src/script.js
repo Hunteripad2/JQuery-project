293243bbdabdf27f094e15e9bd541959eaa9412d
Juju Man <hunteripad2@gmail.com>
2021-02-20 01:03:44 +0300

    Made some fixes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 $("#newtodo_form").on("submit", function (event) {
     const newTodo = $("#newtodo_form_text").val();
-    const newTodoItem = `<div class="d-flex flex-row w-auto mb-3"><div class="col-6"><input type="text" class="datepicker form-control-plaintext text-end" placeholder="Введите дату... "></div><div class="form-check col-auto todo_item" style="margin-top: 2%"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div></div>`;
+    const newTodoItem = `<div class="col-auto d-flex flex-row mt-5 me-auto"><input type="text" class="datepicker form-control-plaintext text-end pe-1" placeholder="Введите дату... "><div class="form-check todo_item mt-2"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div></div>`;
+    const addTodoButton = $("#newtodo_form_button");
 
     if ($(".todo_item").length % 2 === 0) {
         $("#todo_list").append("<div class='w-100'></div>");
@@ -8,9 +9,19 @@ $("#newtodo_form").on("submit", function (event) {
 
     $("#todo_list").append(newTodoItem);
     $("#newtodo_form_text").val("");
-	$(".datepicker").datepicker();
+    $(".datepicker").datepicker();
+    addTodoButton.prop("disabled", true);
 
     event.preventDefault();
+});
+
+$("#newtodo_form").on("change", function () {
+    const newTodo = $("#newtodo_form_text").val();
+    const addTodoButton = $("#newtodo_form_button");
+
+    if (newTodo) {
+        addTodoButton.prop("disabled", false);
+    } else addTodoButton.prop("disabled", true);
 });
 
 function handleChange(checkBox) {
