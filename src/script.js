36c72e7adc4a58a71cc93e93d75e3a013d2ed220
Juju Man <hunteripad2@gmail.com>
2021-03-11 21:52:04 +0300

    Made some minor fixes

--- src/script.js
+++ src/script.js
@@ -1,27 +1,34 @@
 $("#newtodo_form").on("submit", function (event) {
-    const newTodo = $("#newtodo_form_text").val();
-    const newTodoItem = `<div class="col-auto d-flex flex-row mt-5 me-auto"><input type="text" class="datepicker form-control-plaintext text-end pe-1" placeholder="Введите дату... "><div class="form-check todo_item mt-2"><input type="checkbox" onchange="handleChange(this)" class="form-check-input"><label class="form-check-label">${newTodo}</label></div></div>`;
-    const addTodoButton = $("#newtodo_form_button");
+    event.preventDefault();
 
-    if ($(".todo_item").length % 2 === 0) {
-        $("#todo_list").append("<div class='w-100'></div>");
+    const newTodoText = $("#newtodo_form_text").val();
+    const newTodoItem = `<div class="col-auto d-flex flex-row mt-5 me-auto ms-auto todo_item">
+							<input type="text" class="datepicker form-control-plaintext text-end pe-1" placeholder="Введите дату... ">
+							<div class="form-check mt-2"><input type="checkbox" onchange="handleChange(this)" class="form-check-input">
+								<label class="form-check-label">${newTodoText}</label>
+							</div>
+						</div>`;
+    const newTodoButton = $("#newtodo_form_button");
+    const todoList = $("#todo_list");
+    const todoItemList = $(".todo_item");
+
+    if (todoItemList.length % 2 === 0) {
+        todoList.append("<div class='w-100'></div>");
     }
 
-    $("#todo_list").append(newTodoItem);
+    todoList.append(newTodoItem);
     $("#newtodo_form_text").val("");
     $(".datepicker").datepicker();
-    addTodoButton.prop("disabled", true);
-
-    event.preventDefault();
+    newTodoButton.prop("disabled", true);
 });
 
 $("#newtodo_form").on("change", function () {
-    const newTodo = $("#newtodo_form_text").val();
-    const addTodoButton = $("#newtodo_form_button");
+    const newTodoText = $("#newtodo_form_text").val();
+    const newTodoButton = $("#newtodo_form_button");
 
-    if (newTodo) {
-        addTodoButton.prop("disabled", false);
-    } else addTodoButton.prop("disabled", true);
+    if (newTodoText) {
+        newTodoButton.prop("disabled", false);
+    } else newTodoButton.prop("disabled", true);
 });
 
 function handleChange(checkBox) {
